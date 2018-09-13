38e519a40c8ee2f3f220c25d02c702e506cae29f
Arifullah Jan <ajan.bscs16seecs@seecs.edu.pk>
2018-09-13 16:21:52 +0500

    intial project with errors

---
+++ MatrixLib/MatrixAddSub.cpp
@@ -0,0 +1,14 @@
+
+
+#include "MatrixLib.h"
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+
+using namespace std;
+
+
+vector<vector<int>>  MatrixLib::MatrixAddSub::add(vector<vector<int>> a, vector<vector<int>>b, bool add){
+
+	return a;
+}
