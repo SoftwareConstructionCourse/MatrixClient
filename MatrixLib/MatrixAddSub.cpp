57b24b56d5ed18053bb617008fd4af9b1fd32545
wy038 <wyaseen.bscs16seecs@seecs.edu.pk>
2018-09-13 16:55:19 +0500

    add and subtract

--- MatrixLib/MatrixAddSub.cpp
+++ MatrixLib/MatrixAddSub.cpp
@@ -12,3 +12,54 @@ vector<vector<int>>  MatrixLib::MatrixAddSub::add(vector<vector<int>> a, vector<
 
 	return a;
 }
+
+vector<vector<int> > adsdafsdd(vector<vector<int> > m1, vector<vector<int> > m2){
+	// check order
+	vector<vector<int> > result;
+	//sameorder(m1,m2)
+	int r1 = m1.size();
+	int r2 = m2.size();
+	int c1 = m1[0].size();
+	int c2 = m2[0].size();
+	//If(r1==r2&&c1==c2)
+	{
+		for (int i = 0; i<rows; i++)
+		{
+			for (int j = 0; j<columns; j++)
+			{
+				cout << "Enter the number for Matrix 1";
+				result[i][j] = m1[i][j] + m2[i][j];
+			}
+
+
+		}
+
+
+	}
+
+
+}
+
+vector<vector<int> > suasdfsdb(vector<vector<int> > m1, vector<vector<int> > m2){
+	// check order
+	vector<vector<int> > result;
+	//sameorder(m1,m2)
+	//If(sameorder)
+	{
+		for (int i = 0; i<rows; i++)
+		{
+			for (int j = 0; j<columns; j++)
+			{
+				cout << "Enter the number for Matrix 1";
+				result[i][j] = m1[i][j] + m2[i][j];
+			}
+
+
+		}
+		return  result;
+
+	}
+
+
+}
+
